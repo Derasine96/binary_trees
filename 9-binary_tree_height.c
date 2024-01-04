8060efd5570f68bfbd719e22504ed2dede059d01
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-01-03 19:57:28 -0800

    function that measures the height of a binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 		leftside = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		rightside  = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((leftsie > rightside) ? l : r);
+		return ((leftside > rightside) ? l : r);
 	}
 	return (0);
 }
